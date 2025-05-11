9b110136dfaeaf111daf1b0f3f05d09f59161df7
Brian Augnoo <augnoobrian@gmail.com>
2025-05-11 16:57:48 +0400

    add relation, validates, seed to car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,5 @@
 class Car < ApplicationRecord
   belongs_to :user
+  validates :make, :model, :colour, :year, presence: true, allow_blank: false
+  validates :description, length: { minimum: 50, message: "the description should have min 50 chars" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "destroy all the users"
+User.destroy_all
+puts "create user brian"
+brian = User.create!(email: "brianaugnoo@gmail.com", password: "baugnoo", password_confirmation: "baugnoo")
+puts "the user are created"
+
+puts "destroy all car"
+Car.destroy_all
+puts "create Brian car"
+description = "very cool car, lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem"
+brian_car = Car.new(make: "peugeot", model: "i don't now", colour: "black", year: 2006, description: description)
+brian_car.user = brian
+result = brian_car.save ? "the car are saved" : "the car are not saved"
+puts result
