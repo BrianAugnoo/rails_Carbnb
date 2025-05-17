7e63dcb5856e276192af51db091c2ebb4a6e09a1
Brian Augnoo <augnoobrian@gmail.com>
2025-05-18 00:08:05 +0400

    set up the nav_bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#index"
+  get "account" => "home#account", as: :account
   resources :cars do
     resources :bookings, only: [ :new, :create ]
     resources :reviews, only: [ :new, :create ]
