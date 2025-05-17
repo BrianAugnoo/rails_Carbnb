07ec411cdbd82bf81ee98f052f7bde88afbd88c8
Brian Augnoo <augnoobrian@gmail.com>
2025-05-17 22:20:44 +0400

    configure bootstrap js

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,5 @@
 
 pin "application", preload: true
 pin "bootstrap" # @5.3.6
+pin "bootstrap", to: "bootstrap/dist/js/bootstrap.bundle.min.js"
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
