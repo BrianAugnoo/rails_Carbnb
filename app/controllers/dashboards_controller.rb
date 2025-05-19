fb0c7009238f27dc9c3ca7295c2eb9c73c00fc03
Brian Augnoo <augnoobrian@gmail.com>
2025-05-19 17:30:52 +0400

    add accept action to booking

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,12 @@ class DashboardsController < ApplicationController
   def index
     @cars = current_user.cars
     @bookings = Booking.all
+    @my_bookings = current_user.bookings
     @user_bookings = []
 
     @cars.each do |car|
       car.bookings.each do |booking|
-          @user_bookings << booking
+          @user_bookings << booking if booking[:status] == false
       end
     end
   end
