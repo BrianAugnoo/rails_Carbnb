2481ca1c5d18b5d083c6de4a9f84c515ee6f49db
Brian Augnoo <augnoobrian@gmail.com>
2025-05-22 14:24:27 +0400

    ajax request succesful connected to controller

--- app/javascript/controllers/create_notification_controller.js
+++ app/javascript/controllers/create_notification_controller.js
@@ -2,11 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="create-notification"
 export default class extends Controller {
-  connect() {
-    console.log("CreateNotificationController connected")
+  static targets = ["userDelete"]
+  userCancel(event) {
+    event.preventDefault()
+    if (confirm("Are you sure you want to cancel?")) {
+      const bookingId = this.userDeleteTarget.getAttribute("booking-id")
+      this.delete_request(`/bookings/${bookingId}`, JSON.stringify({ id: bookingId }))
+    }
   }
 
-  confirm() {
-    console.log("Confirm button clicked")
+  csrf_token() {
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute("content")
+    return token
+  }
+
+  delete_request(action, body){
+    fetch(action, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": this.csrf_token(),
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: body
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("Network response was not ok")
+      }
+    })
+    .then(data => data)
   }
 }
